0518b46e9274e1b5270dd32b067ba39337e772ae
Andrii <marchenkoandy@gmail.com>
2018-04-02 22:03:25 +0300

    lesson5 enums

---
+++ src/main/java/com/company/lesson5/SimpleEnum.java
@@ -0,0 +1,16 @@
+package com.company.lesson5;
+
+public enum SimpleEnum {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
